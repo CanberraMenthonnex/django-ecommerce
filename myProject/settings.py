16ceec32f7a83b23f203aa23ea93c4dfb2803ace
Lucas <lucas.daval@edu.devinci.fr>
2022-02-28 16:07:30 +0100

    init urls + link with project

--- myProject/settings.py
+++ myProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shopApp.apps.ShopappConfig',
     'firstApp.apps.FirstappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
