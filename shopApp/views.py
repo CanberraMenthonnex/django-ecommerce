2d4592c9d6305d8d003ac4e6c9cd4943dac0c17b
Canberra <canberra.mc@gmail.com>
2022-03-01 09:24:26 +0100

    list product

--- shopApp/views.py
+++ shopApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'shopApp/index.html')
+    context ={}
+    # add the dictionary during initialization
+    context["dataset"] = Product.objects.all()
+         
+    return render(request, "shopApp/index.html", context)
+
